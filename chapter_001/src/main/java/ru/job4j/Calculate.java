f4c28c83c9d92bc3cb0d331a7131b880b1c55585
Gleb <kuznetsovge@yandex.ru>
2017-09-13 18:42:37 +0500

    Setup IntellijIDE [4417]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,24 +1,24 @@
 package ru.job4j;
 /**
-*Class класс для вывода сообщения "Hello world" в консоль.
-*
-*@author gkuznetsov.
-*@since 13.09.17.
-*@version 0.1.
-*/
+ *Class класс для вывода сообщения "Hello world" в консоль.
+ *
+ *@author gkuznetsov.
+ *@since 13.09.17.
+ *@version 0.1.
+ */
 class Calculate {
 	/**
-	*Начало выполнения программы. Вывод сообщения "Hello world" в консоль.
-	*@param args - массив аргументов args.
-	*/
+	 *Начало выполнения программы. Вывод сообщения "Hello world" в консоль.
+	 *@param args - массив аргументов args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
 	/**
-	Method echo.
-	*@param name Your name.
-	*@return Echo plus your name.
-	*/
+	 Method echo.
+	 *@param name Your name.
+	 *@return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}

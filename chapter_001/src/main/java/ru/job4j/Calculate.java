29719fd097bd6951edf9614738f4b7539f0c9bb8
Gleb <kuznetsovge@yandex.ru>
2017-09-13 13:33:05 +0500

    Add description to Calculate [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
-
+/**
+* Class класс для вывода сообщения "Hello world" в консоль
+* @author gkuznetsov
+* @since 13.09.17
+* @version 0.1
+*/
 class Calculate {
+	
+	/**
+	* Начало выполнения программы. Вывод сообщения "Hello world" в консоль
+	* @param arg - args массив аргументов args
+	* @return void ничего не возвращает
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/packege-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+*@author Gleb Kuznetsov
+*@since 13.09.2017
+*@version 0.1
+*/
+package ru.job4j;

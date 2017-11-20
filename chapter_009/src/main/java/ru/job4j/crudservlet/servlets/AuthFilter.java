aa266cdc6e2a314102b016c5575f99c884b0d17b
Глеб Кузнецов <g.kuznetsov@xrm.local>
2017-11-20 10:54:02 +0500

    Fix sinchronized at AuthFilter [#2513]

--- chapter_009/src/main/java/ru/job4j/crudservlet/servlets/AuthFilter.java
+++ chapter_009/src/main/java/ru/job4j/crudservlet/servlets/AuthFilter.java
@@ -24,6 +24,7 @@ import javax.servlet.ServletResponse;
  * @since 07.11.2017.
  */
 public class AuthFilter implements Filter {
+
     /**
      * Logger.
      */
@@ -50,15 +51,14 @@ public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
+        final HttpSession session = request.getSession();
+
         if (request.getRequestURI().contains("/signin")) {
             chain.doFilter(req, resp);
         } else {
-            HttpSession session =  request.getSession();
-            synchronized (session) {
-                if (session.getAttribute("login") == null) {
-                    ((HttpServletResponse) resp).sendRedirect(String.format("%s/signin", request.getContextPath()));
-                    return;
-                }
+            if (session.getAttribute("login") == null) {
+                ((HttpServletResponse) resp).sendRedirect(String.format("%s/signin", request.getContextPath()));
+                return;
             }
             chain.doFilter(req, resp);
         }
